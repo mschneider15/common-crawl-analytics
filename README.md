d1ce251ba246c04cdd24a6e90b52125e7c35bdc3
swiminspace <andre.j.michell2@gmail.com>
2018-02-20 09:36:14 -0500

    Update README with instructions for local run. Update SimpleApp.scala with a more simple REGEX option, improved write setup (works on local system).

--- README.md
+++ README.md
@@ -20,6 +20,11 @@ This project was developed on the DoD HPC Center Topaz cluster. To get an accoun
 * To build the jar, download sbt-1.0.2 and run `sbt package`
 * Copy the jar to `$WORKDIR` and edit the last line of `spark-submit.pbs` with your jar location, then run:
 
+* On a personal installation:
+* Run `sbt package` in the root directory of this project
+* Identify the path to the jar file. It is likely in the /target directory and starts with `cc-2.11...`
+* Run `spark-submit --class edu.usma.cc.SimpleApp \path-to-jar-file.jar`
+
 ```bash
 $ qsub spark-submit.pbs 
 ```
--- src/main/scala/edu/usma/cc/SimpleApp.scala
+++ src/main/scala/edu/usma/cc/SimpleApp.scala
@@ -2,20 +2,26 @@ package edu.usma.cc
 
 import java.io._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.rdd.RDD
 import scala.util.matching.Regex
 
 object SimpleApp {
   def main(args: Array[String]) {
     val logFile = "/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-data/CC-MAIN-2018-05/segments/1516084891105.83/wet/CC-MAIN-20180122054202-20180122074202-00449.warc.wet"
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
+    val logData = spark.read.textFile(logFile)
 
-    val emailPattern = new Regex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
+    val emailPattern = new Regex("""[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}""")
+//  val emailPattern = new Regex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
 
-    val emails = logData.filter(line => emailPattern.findFirstMatchIn(line).getOrElse("None.") != "None.")
-    emails.collect()
-    emails.saveAsTextFile("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
+//    for (line <- logData) {
+//      val m = emailPattern.findFirstMatchIn(line).getOrElse("none")
+//      if (m != "none") println(m)
+//    }
+
+    val emails = logData.flatMap(line => emailPattern.findFirstMatchIn(line))
+    emails.write.format("text").save("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
+//    emails.collect()
+//    emails.saveAsTextFile("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
     spark.stop()
   }
 }
