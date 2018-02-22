1765479a3b30a8640298382b097cb4e4d46115f3
swiminspace <andre.j.michell2@gmail.com>
2018-02-22 09:39:37 -0500

    Update README.md. Update SimpleApp with more REGEX options and currently outputs (when running the more simple email REGEX) only the emails on each line into the emails.txt file within the crawl-results directory

--- README.md
+++ README.md
@@ -17,10 +17,13 @@ This project was developed on the DoD HPC Center Topaz cluster. To get an accoun
 
 ## Running Jobs
 
+### On a Cluster
+
 * To build the jar, download sbt-1.0.2 and run `sbt package`
 * Copy the jar to `$WORKDIR` and edit the last line of `spark-submit.pbs` with your jar location, then run:
 
-* On a personal installation:
+### On a personal installation:
+
 * Run `sbt package` in the root directory of this project
 * Identify the path to the jar file. It is likely in the /target directory and starts with `cc-2.11...`
 * Run `spark-submit --class edu.usma.cc.SimpleApp \path-to-jar-file.jar`
@@ -28,3 +31,15 @@ This project was developed on the DoD HPC Center Topaz cluster. To get an accoun
 ```bash
 $ qsub spark-submit.pbs 
 ```
+
+## Spark 2.-.- idiosyncracies
+
+* Using map & flatMap methods on Datasets
+You can no longer map directly over the rows of DataFrames, as they are now simply a type alias for Dataset[Row]. In Spark 2, to map over a Dataset[T], an encoder for type T must be available in the current context. Encoders are available for standard types and for case classes, but not for Row since it is a generic container type. If you wish to map over the rows of a DataFrame df, you should now convert it to an RDD first:
+
+* Where before you would write:
+
+`df.map { row: Row  => ... }`
+
+`df.rdd.map { row: Row => ... }`
+
--- src/main/scala/edu/usma/cc/SimpleApp.scala
+++ src/main/scala/edu/usma/cc/SimpleApp.scala
@@ -9,19 +9,21 @@ object SimpleApp {
     val logFile = "/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-data/CC-MAIN-2018-05/segments/1516084891105.83/wet/CC-MAIN-20180122054202-20180122074202-00449.warc.wet"
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile)
+    
+    // The following are a few different REGEXs which can be used in finding emails
+    // emailPattern is the most simple
+//    val emailPattern = new Regex("""[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}""")
+    // emailPattern1 is the most complex. It is prone to a stackoverflow error due to its complexity
+    val emailPattern1 = new Regex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]){6,64}""")
+    // emailPattern2 is another simple REGEX
+//    val emailPattern2 = new Regex("""([\\w-+]+(?:\\.[\\w-+]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7})""")
+    // emailPattern3 is a REGEX which would, in theory, be used to filter out words greater than 64 characters which do not contain an @ symbol
+//    val emailPattern3 = new Regex("""(?:[a-z0-9._%+-]+@[A-Z0-9.-]\.[A-Z]){8,64}""")
 
-    val emailPattern = new Regex("""[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}""")
-//  val emailPattern = new Regex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
-
-//    for (line <- logData) {
-//      val m = emailPattern.findFirstMatchIn(line).getOrElse("none")
-//      if (m != "none") println(m)
-//    }
-
-    val emails = logData.flatMap(line => emailPattern.findFirstMatchIn(line))
-    emails.write.format("text").save("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
+    val emails = logData.rdd.flatMap(line => emailPattern1.findFirstMatchIn(line))
+    //emails.write.format("text").save("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
 //    emails.collect()
-//    emails.saveAsTextFile("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
+    emails.saveAsTextFile("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
     spark.stop()
   }
 }
