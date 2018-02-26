1cf6dd082a421d4170b6ae3ba40dc38012da05e5
swiminspace <andre.j.michell2@gmail.com>
2018-02-26 08:21:06 -0500

    emailPattern4 catching 12136 emails when run on the specified WET file contained within.

--- src/main/scala/edu/usma/cc/SimpleApp.scala
+++ src/main/scala/edu/usma/cc/SimpleApp.scala
@@ -12,15 +12,16 @@ object SimpleApp {
     
     // The following are a few different REGEXs which can be used in finding emails
     // emailPattern is the most simple
-//    val emailPattern = new Regex("""[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}""")
+    // val emailPattern = new Regex("""[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}""")
     // emailPattern1 is the most complex. It is prone to a stackoverflow error due to its complexity
-    val emailPattern1 = new Regex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]){6,64}""")
+    // val emailPattern1 = new Regex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]){6,64}""")
     // emailPattern2 is another simple REGEX
-//    val emailPattern2 = new Regex("""([\\w-+]+(?:\\.[\\w-+]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7})""")
+    // val emailPattern2 = new Regex("""([\\w-+]+(?:\\.[\\w-+]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7})""")
     // emailPattern3 is a REGEX which would, in theory, be used to filter out words greater than 64 characters which do not contain an @ symbol
-//    val emailPattern3 = new Regex("""(?:[a-z0-9._%+-]+@[A-Z0-9.-]\.[A-Z]){8,64}""")
-
-    val emails = logData.rdd.flatMap(line => emailPattern1.findFirstMatchIn(line))
+    // val emailPattern3 = new Regex("""(?:[a-z0-9._%+-]+@[A-Z0-9.-]\.[A-Z]){8,64}""")
+    // emailPattern4 is a REGEX which is simpler, yet ought still catch many, if not all emails
+    val emailPattern4 = new Regex("""\b[A-Za-z0-9._%+-]{1,64}@(?:[A-Za-z0-9.-]{1,63}\.){1,125}[A-Za-z]{2,63}\b""")
+    val emails = logData.rdd.flatMap(line => emailPattern4.findFirstMatchIn(line))
     //emails.write.format("text").save("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
 //    emails.collect()
     emails.saveAsTextFile("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
