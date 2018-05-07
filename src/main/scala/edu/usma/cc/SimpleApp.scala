67d3305c49998a08da631beaced6fb64e3e9f9ea
swiminspace <andre.j.michell2@gmail.com>
2018-05-07 09:35:25 -0400

    Setup the com.martinkl.warc package to retrieve emails from the record's content string.

--- src/main/scala/edu/usma/cc/SimpleApp.scala
+++ src/main/scala/edu/usma/cc/SimpleApp.scala
@@ -22,55 +22,70 @@ import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.sql.SparkSession
-import org.apache.hadoop.io.LongWritable
+
+import org.apache.hadoop.io._
 
 object SimpleApp {
 
   def main(args: Array[String]) {
 		// Path to WARC files
 		// Directory paths will allow access to all files within	
-  	val warcFile = "/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-data/CC-MAIN-2018-05/segments/1516084887660.30/wet/CC-MAIN-20180118230513-20180119010513-00000.warc.wet.gz"
+  	val warcFile = "/Users/andre/Documents/Academics/AY18-2/cs489A/CC-MAIN-20180118230513-20180119010513-00000.warc.wet"
     val firstDir = "/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-data/CC-MAIN-2018-05/segments/1516084887660.30/wet/*"
     val secondDir = "/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-data/CC-MAIN-2018-05/segments/1516084891105.83/wet/*"
 		
 		// Initialize the sparkSession
 		val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val sc = spark.sparkContext
 		
-    // Initialize a StreamingContext, ssc
-    val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
 
 		// Open warcFile as an input stream
-    // format: fileStream[keyClass, valueClass, InputFormatClass](filePath)
-    val in = ssc.fileStream[LongWritable,WARCWritable,WARCInputFormat](warcFile) 
+    val warcInput = sc.newAPIHadoopFile(warcFile, classOf[WARCInputFormat], classOf[LongWritable],classOf[WARCWritable]) 
 
 		// Read in the file directories for analysis
     // val logData = spark.read.textFile(firstDir, secondDir)
     
-		// Defines the Regex used in finding emails
-    // val emailPattern = new Regex("""\b[A-Za-z0-9._%+-]{1,64}@(?:[A-Za-z0-9.-]{1,63}\.){1,125}[A-Za-z]{2,63}\b""")
-    
-		// Find all of the emails in each line of the warc files
-		// val emails = logData.rdd.flatMap{line => emailPattern.findAllMatchIn(line)}
     
     // Save all emails as a text file in the following directory.
     // TODO - Collect results so they are automatically stored in a single file
     // emails.saveAsTextFile("/Users/andre/Documents/Academics/AY18-2/cs489A/crawl-results/emails.txt")
 
+    val warcs = warcInput.values
+
+    val test1Result = warcs.map( warc => analyze(warc.getRecord) )
+
     // Initiate the records information
 
     println("--------------")
     println("WARCRecords:")
-    val urls = in.mapValues(
-        record => (record.getRecord.getHeader.getTargetURI, 1)
-    )
-    //val urlsCounts = urls.reduceByKey(_ + _)
-    //urlsCounts.print()
-    urls.print()
-    ssc.start()
-    ssc.awaitTermination()
-    // Shut down the Spark session
-    if ssc.stop()
-    // spark.stop()
+    test1Result.foreach(println)
+    spark.stop()
+  }
+
+  def analyze(record: WARCRecord): String = {
+    val ret = returnEmails(record)
+    return ret
+  }
+
+  def returnEmails(record: WARCRecord): String = {
+		// Defines the Regex used in finding emails
+    val emailPattern = new Regex("""\b[A-Za-z0-9._%+-]{1,64}@(?:[A-Za-z0-9.-]{1,63}\.){1,125}[A-Za-z]{2,63}\b""")
+    
+    val content = new String(record.getContent)
+
+    var ret = ""
+    emailPattern.findAllMatchIn(content).map(_ + "\n").foreach(ret += _)
+    return ret
+  }
+
+  def returnRecordHeader(record: WARCRecord): String = {
+    var ret = "--------------\n"
+    ret += "     Record-ID: " + record.getHeader.getRecordID + "\n"
+    ret += "          Date: " + record.getHeader.getDateString + "\n"
+    ret += "Content-Length: " + record.getHeader.getContentLength + "\n"
+    ret += "  Content-Type: " + record.getHeader.getContentType + "\n"
+    ret += "     TargetUri: " + record.getHeader.getTargetURI + "\n"
+    return ret
   }
 
   def printRecordHeader(record: WARCRecord) {
